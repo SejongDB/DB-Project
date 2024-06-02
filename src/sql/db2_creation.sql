41d54e255287a13cb7f4d01e3eee2e581b1bd695
Subin <rla0010101@gmail.com>
2024-06-02 10:38:07 +0900

    db_creation.sql 조건에 따라 재수정

--- src/sql/db2_creation.sql
+++ src/sql/db2_creation.sql
@@ -4,61 +4,52 @@ CREATE DATABASE db2;
 USE db2;
 
 CREATE TABLE `movie` (
-	`movie_id`	INT	NOT NULL,
-	`movie_name`	varchar(255)	NULL,
-	`showtime`	int4	NULL,
-	`rating`	varchar(255)	NULL,
-	`director`	varchar(255)	NULL,
-	`actor`	varchar(255)	NULL,
-	`genre`	varchar(255)	NULL,
-	`instruction`	varchar(512)	NULL,
-	`movie_created_at`	datetime	NULL,
-	`grade`	float	NULL
-);
-
-CREATE TABLE `schedule` (
-	`schedule_id`	INT	NOT NULL,
-	`created_at`	datetime	NULL,
-	`screening_day`	varchar(255)	NULL,
-	`screening_count`	int4	NULL,
-	`start_time`	datetime	NULL,
-	`end_time`	datetime	NULL,
-	`movie_id`	INT	NOT NULL,
-	`theater_id`	INT	NOT NULL
+	`movie_id` INT NOT NULL AUTO_INCREMENT,
+	`movie_name` VARCHAR(255) NULL,
+	`showtime` INT NULL,
+	`rating` VARCHAR(255) NULL,
+	`director` VARCHAR(255) NULL,
+	`actor` VARCHAR(255) NULL,
+	`genre` VARCHAR(255) NULL,
+	`instruction` VARCHAR(512) NULL,
+	`movie_created_at` DATETIME NULL,
+	`grade` FLOAT NULL,
+	PRIMARY KEY (`movie_id`)
 );
 
 CREATE TABLE `theater` (
-	`theater_id`	INT	NOT NULL,
-	`theater_availability`	tinyint(1)	NULL,
-	`row_seat`	int4	NULL,
-	`column_seat`	int4	NULL,
-	`total_seat`	int4	NULL
+	`theater_id` VARCHAR(50) NOT NULL,
+	`theater_availability` TINYINT(1) NULL,
+	`row_seat` INT NULL,
+	`column_seat` INT NULL,
+	`total_seat` INT NULL,
+	PRIMARY KEY (`theater_id`)
 );
+
+CREATE TABLE `schedule` (
+	`schedule_id` INT NOT NULL AUTO_INCREMENT,
+	`created_at` DATETIME NULL,
+	`screening_day` VARCHAR(255) NULL,
+	`screening_count` INT NULL,
+	`start_time` DATETIME NULL,
+	`end_time` DATETIME NULL,
+	`movie_id` INT NOT NULL,
+	`theater_id` VARCHAR(50) NOT NULL,
+	PRIMARY KEY (`schedule_id`),
+	FOREIGN KEY (`movie_id`) REFERENCES `movie`(`movie_id`),
+	FOREIGN KEY (`theater_id`) REFERENCES `theater`(`theater_id`)
+);
+
+
 
 CREATE TABLE `seat` (
-	`seat_id`	INT	NOT NULL,
-	`seat_availability`	tinyint(1)	NULL,
-	`theater_id`	INT	NOT NULL
-);
-
-CREATE TABLE `ticket` (
-	`ticket_id`	INT	NOT NULL,
-	`ticket_availibility`	tinyint(1)	NULL,
-	`standard_price`	int4	NULL,
-	`sale_price`	int4	NULL,
-	`seat_id`	INT	NOT NULL,
-	`reservation_id`	INT	NOT NULL,
-	`schedule_id`	INT	NOT NULL,
-	`theater_id`	INT	NOT NULL
-);
-
-CREATE TABLE `reservation` (
-	`reservation_id`	INT	NOT NULL,
-	`payment_method`	varchar(255)	NULL,
-	`payment_amount`	int4	NULL,
-	`payment_status`	varchar(255)	NULL,
-	`payment_date`	datetime	NULL,
-	`member_id`	INT	NOT NULL
+	`seat_id` INT NOT NULL AUTO_INCREMENT,
+	`theater_id` VARCHAR(50) NOT NULL,
+	`row` INT NULL,
+	`column` INT NULL,
+	`seat_availability`	tinyint(1)	NOT NULL,
+	PRIMARY KEY (`seat_id`),
+	FOREIGN KEY (`theater_id`) REFERENCES `theater`(`theater_id`)
 );
 
 CREATE TABLE `member` (
@@ -66,36 +57,33 @@ CREATE TABLE `member` (
 	`name`	varchar(255)	NULL,
 	`phonenumber`	varchar(255)	NULL,
 	`email`	varchar(255)	NULL,
-	`role`	varchar(255)	NULL
+	`role`	varchar(255)	NULL,
+    PRIMARY KEY (`member_id`)
 );
 
-
-ALTER TABLE `movie` ADD CONSTRAINT `PK_MOVIE` PRIMARY KEY (
-	`movie_id`
+CREATE TABLE `reservation` (
+	`reservation_id` INT NOT NULL AUTO_INCREMENT,
+	`payment_method`	varchar(255) NOT NULL,
+	`payment_amount`	int4 NOT NULL,
+	`payment_status`	varchar(255)	NOT NULL,
+	`payment_date`	datetime NOT NULL,
+	`member_id` INT NOT NULL,
+	PRIMARY KEY (`reservation_id`),
+	FOREIGN KEY (`member_id`) REFERENCES `member`(`member_id`)
 );
 
-ALTER TABLE `schedule` ADD CONSTRAINT `PK_SCHEDULE` PRIMARY KEY (
-	`schedule_id`
+CREATE TABLE `ticket` (
+	`ticket_id` INT NOT NULL AUTO_INCREMENT,
+	`ticket_availibility`	tinyint(1)	NOT NULL,
+	`standard_price`	int4	NOT NULL,
+	`sale_price`	int4	NOT NULL,
+	`seat_id` INT NOT NULL,
+	`reservation_id` INT NOT NULL,
+	`schedule_id` INT NOT NULL,
+	`theater_id` VARCHAR(50) NOT NULL,
+	PRIMARY KEY (`ticket_id`),
+	FOREIGN KEY (`reservation_id`) REFERENCES `reservation`(`reservation_id`),
+	FOREIGN KEY (`schedule_id`) REFERENCES `schedule`(`schedule_id`),
+	FOREIGN KEY (`seat_id`) REFERENCES `seat`(`seat_id`),
+	FOREIGN KEY (`theater_id`) REFERENCES `theater`(`theater_id`)
 );
-
-ALTER TABLE `theater` ADD CONSTRAINT `PK_THEATER` PRIMARY KEY (
-	`theater_id`
-);
-
-ALTER TABLE `seat` ADD CONSTRAINT `PK_SEAT` PRIMARY KEY (
-	`seat_id`
-);
-
-ALTER TABLE `ticket` ADD CONSTRAINT `PK_TICKET` PRIMARY KEY (
-	`ticket_id`
-);
-
-ALTER TABLE `reservation` ADD CONSTRAINT `PK_RESERVATION` PRIMARY KEY (
-	`reservation_id`
-);
-
-ALTER TABLE `member` ADD CONSTRAINT `PK_MEMBER` PRIMARY KEY (
-	`member_id`
-);
-
-
