a76784317bc15307ed5b704625ed4f51615a78da
Subin <rla0010101@gmail.com>
2024-06-02 15:47:09 +0900

    회원이 입력되지 않은 정보는 무시하고 모든 영화에 대한 조회가 가능한 DELIMITER 구현

---
+++ src/sql/user/user_searchMovie.sql
@@ -0,0 +1,51 @@
+USE db2;
+
+DROP PROCEDURE IF EXISTS CheckUserRole;
+DELIMITER //
+CREATE PROCEDURE CheckUserRole(
+    IN input_member_id INT,
+    OUT is_user_role BOOLEAN
+)
+BEGIN
+    DECLARE user_role VARCHAR(255);
+    
+    SELECT role INTO user_role
+    FROM member
+    WHERE member_id = input_member_id;
+    
+    IF user_role = 'USER' THEN
+        SET is_user_role = TRUE;
+    ELSE
+        SET is_user_role = FALSE;
+    END IF;
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS SearchMovies;
+DELIMITER //
+CREATE PROCEDURE SearchMovies(
+    IN input_member_id INT,
+    IN movie_title VARCHAR(255),
+    IN director_name VARCHAR(255),
+    IN actor_name VARCHAR(255),
+    IN genre_name VARCHAR(255)
+)
+BEGIN
+    DECLARE is_user_role BOOLEAN;
+    
+    CALL CheckUserRole(input_member_id, is_user_role);
+    
+    IF is_user_role THEN
+        SELECT m.movie_name AS 영화명, m.director AS 감독명, m.actor AS 배우명, m.genre AS 장르
+        FROM movie m
+        WHERE (movie_title IS NULL OR m.movie_name LIKE CONCAT('%', movie_title, '%'))
+          AND (director_name IS NULL OR m.director LIKE CONCAT('%', director_name, '%'))
+          AND (actor_name IS NULL OR m.actor LIKE CONCAT('%', actor_name, '%'))
+          AND (genre_name IS NULL OR m.genre LIKE CONCAT('%', genre_name, '%'));
+    ELSE
+        SELECT 'USER 회원 권한인 사용자만 해당 쿼리를 실행할 수 있습니다.' AS Message;
+    END IF;
+END //
+DELIMITER ;
+
+CALL SearchMovies(1, '무야호', NULL, NULL, NULL);
