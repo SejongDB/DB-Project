333fc39742baab81190fc55fcc5f18a7d0b39ff6
Subin <rla0010101@gmail.com>
2024-06-03 14:36:39 +0900

    #4 관리자 로그인 시 모든 권한 부여 기능 추가

--- src/sql/admin/admin_login.sql
+++ src/sql/admin/admin_login.sql
@@ -1,5 +1,15 @@
 USE db2;
 
+DELIMITER //
+DROP PROCEDURE IF EXISTS GrantAdminPrivileges;
+CREATE PROCEDURE GrantAdminPrivileges()
+BEGIN
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+    FLUSH PRIVILEGES;
+END //
+DELIMITER ;
+
+
 DROP PROCEDURE IF EXISTS LoginAdmin;
 
 -- root/1234 로그인 시 자동 관리자 계정 member_id 0 할당되도록
@@ -10,6 +20,7 @@ BEGIN
     
     IF input_username = 'root' AND input_password = '1234' THEN
         SET member_id = 0;
+        CALL GrantAdminPrivileges();
     ELSE
         SET member_id = NULL;
     END IF;
@@ -17,6 +28,8 @@ BEGIN
     SET output_admin_id = member_id;
 END //
 DELIMITER ;
+
+
 
 SET @output_admin_id = NULL;
 CALL LoginAdmin('root', '1234', @output_admin_id);
