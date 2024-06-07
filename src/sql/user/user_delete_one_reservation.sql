3a1137e2e41e72e0fef60bd7fc4aa6d493ef192c
Subin <rla0010101@gmail.com>
2024-06-07 21:12:51 +0900

    #5 회원 단건 예매 내역 삭제 시 회원의 전체 예매 내역이 삭제되는 에러 해결

--- src/sql/user/user_delete_one_reservation.sql
+++ src/sql/user/user_delete_one_reservation.sql
@@ -6,31 +6,26 @@ DELIMITER //
 
 CREATE PROCEDURE deleteUserReservation(
     IN input_member_id INT,
-    IN input_reservation_id INT -- 유저가 본인 예매 내역 조회 후 삭제하고 싶은 예매 내역 id를 념기면 됨
+    IN input_reservation_id INT -- 유저가 본인 예매 내역 조회 후 삭제하고 싶은 예매 내역 id를 넘기면 됨
 )
 BEGIN
+    DECLARE seat_id INT;
 
-	-- 티켓이 삭제된 좌석의 seat_availability를 0으로 변경
-    UPDATE seat s
-    JOIN (
-        SELECT seat_id
-        FROM ticket
-        WHERE reservation_id IN (SELECT reservation_id FROM reservation)
-    ) AS deleted_seats ON s.seat_id = deleted_seats.seat_id
-    SET s.seat_availability = 0;
-    
+    -- 티켓이 삭제된 좌석의 seat_availability를 0으로 변경
+    SELECT t.seat_id INTO seat_id
+    FROM ticket t
+    WHERE t.reservation_id = input_reservation_id;
+
+    UPDATE seat
+    SET seat_availability = 0
+    WHERE seat_id = seat_id;
+
     -- 해당 회원이 선택한 예약 및 연결된 티켓 삭제
-    DELETE FROM reservation WHERE member_id = input_member_id and reservation_id = input_reservation_id;
-    DELETE t FROM ticket t
-    JOIN reservation r ON t.reservation_id = r.reservation_id
-    WHERE r.member_id = input_member_id;
-
-    
+    DELETE FROM ticket WHERE reservation_id = input_reservation_id;
+    DELETE FROM reservation WHERE member_id = input_member_id AND reservation_id = input_reservation_id;
 END //
-
+	
 DELIMITER ;
 
-CALL deleteUserReservation(2,25);
-select * from reservation where reservation_id = 25;
-select * from ticket where ticket_id = 25;
-select * from seat where seat_id = 19;
+— 프로시저 호출 예시
+CALL deleteUserReservation(2, 101);
